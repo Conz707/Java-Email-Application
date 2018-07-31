113e04de8ca1f56cdfa7dddcce528f2b531e8236
user <user@LAPTOP-4SJO4KOR>
2018-07-31 11:17:10 +0100

    Test Commit

---
+++ src/emailappPack/Email.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emailappPack;
+
+/**
+ *
+ * @author user
+ */
+public class Email {
+    
+    //private for encapsulation, prevent direct access
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    
+    //Constructors
+    //getting first and last name
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    //Get the department
+    
+    //Generate random password
+    
+    //Set mailbox capacity
+    
+    //Set alternate email address
+    
+    //Change password
+}
+
+
