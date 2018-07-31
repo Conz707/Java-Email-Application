1d8506ea6f952aba8aa0d0541807a96958bb2750
user <user@LAPTOP-4SJO4KOR>
2018-07-31 12:30:56 +0100

    Creates an email for the user and a random 8 char password~ these details are currently stored locally and wiped every run remember to change to add a GUI

--- src/emailappPack/Email.java
+++ src/emailappPack/Email.java
@@ -22,19 +22,30 @@ public class Email {
     private String alternateEmail;
 
     
-    //Constructors
+    //Constructor
     //getting first and last name
     public Email(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("New email created: " + this.firstName + " " + this.lastName);
+        System.out.println("User: " + this.firstName + " " + this.lastName);
         
         
         //getDepartment
         this.department = setDepartment();
         System.out.println("Select Department Number: " + this.department);
         
-    }
+        //create and assign randomPassword
+        this.password = randomPassword(8);
+        System.out.println(this.password);
+        
+        //Create the email
+        //NOTE - Email should add a number after lastName if user already exists.
+        //Email needs to remove the "." after department if the user department = 0
+        String userEmail = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department.toLowerCase() + ".thecompany.co.uk";
+        System.out.println("Email created: " + userEmail);
+        }
+    
+    //Methods
     
     //Get the department
     private String setDepartment(){
@@ -45,18 +56,35 @@ public class Email {
             case 1:
                 return "Sales";
             case 2: 
-                return "Accounting";
+                return "Accounting";    
             case 3: 
                 return "Development";
             case 0:
-                return "N/A";
+                return "";
             default:
                 break;
         }
-        return null;
+        return "";
     }
     //Generate random password
-    
+    private String randomPassword(int passLength){
+        
+        //possible characters
+        String passwordChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        //fill char array with (passLength) number of Chars
+        char[] password = new char[passLength];        
+        
+        //get random character from password & passwordChars (passLength) number of times
+        for(int i = 0; i < passLength; i++){
+            
+            //randValue = random from passChars.length. cast to int
+           int randValue = (int) (Math.random() * passwordChars.length());
+           
+           //add to password array a char from PasswordChars at (randValue) position
+           password[i] = passwordChars.charAt(randValue);
+        }
+        return new String (password);
+    }
     //Set mailbox capacity
     
     //Set alternate email address
