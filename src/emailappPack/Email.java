218c7fc35e1e400f80e37aa3ffedf3fd717f25f3
user <user@LAPTOP-4SJO4KOR>
2018-07-31 11:43:07 +0100

    Creates default user, can select department.

--- src/emailappPack/Email.java
+++ src/emailappPack/Email.java
@@ -5,6 +5,8 @@
  */
 package emailappPack;
 
+import java.util.Scanner;
+
 /**
  *
  * @author user
@@ -25,10 +27,34 @@ public class Email {
     public Email(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.println("New email created: " + this.firstName + " " + this.lastName);
+        
+        
+        //getDepartment
+        this.department = setDepartment();
+        System.out.println("Select Department Number: " + this.department);
+        
     }
     
     //Get the department
-    
+    private String setDepartment(){
+        System.out.print("Departments: \n1 Sales \n2 Accounting \n3 Development \n0 No Department");
+        Scanner in = new Scanner(System.in);
+        int selectedDepartment = in.nextInt();
+        switch(selectedDepartment){
+            case 1:
+                return "Sales";
+            case 2: 
+                return "Accounting";
+            case 3: 
+                return "Development";
+            case 0:
+                return "N/A";
+            default:
+                break;
+        }
+        return null;
+    }
     //Generate random password
     
     //Set mailbox capacity
